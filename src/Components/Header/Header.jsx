b4df86fc6cc984a92ef70729b2f9b42ae5f87305
yassine-ahmed-0803 <yassinoahmed89@gmail.com>
2025-05-02 21:07:28 +0300

    updated the background and the buttons function

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+import logo from "../../assets/images/report_background-removebg-preview.png"
 
 function Header() {
   return (
-    <Navbar bg="light" expand="xxl" className="bg-body-tertiary">
+    <Navbar bg="light" expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="/">Smart Fridge</Navbar.Brand>
+        <Navbar.Brand href="/" src={logo}><img src={logo} style={{width:"40px", height:"40px"}}/>Smart Fridge</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
--- src/Components/MainContent/MainContent.jsx
+++ src/Components/MainContent/MainContent.jsx
@@ -1,21 +1,33 @@
-import React from "react";
+import React , {useState} from "react";
 import "./MainContent.css"
 import { useNavigate } from 'react-router-dom';
 import Fanblades from "../../assets/images/fan_picture-removebg-preview.png"
 
 function MainContent(){
-   const navigate = useNavigate();
+   const navigate = useNavigate();  
+
+   const [count , setcount] = useState(1);
+
+  function increase(){
+    if(count < 4){setcount(count + 1);}
+      
+  }
+  function decrease(){
+    if(count > 1)
+    setcount(count - 1);
+  }
+
     return(
     <div className="fanimage">
           <h1 style={{fontFamily:'josefin slab' , fontSize:'40px' ,  color:'white' , marginBottom:'40px' }}>Refrigerator Temperature</h1>
 
           <img id="rotatingfans" src={Fanblades}/>
 
-          <div className="tempbox">0°C</div>
+          <div className="tempbox">{count}°C</div>
 
           <div className="fanbuttons">
-           <button>+</button>
-           <button>-</button>
+           <button className="increase" onClick={increase}>+</button>
+           <button className="decrease" onClick={decrease}>-</button>
           </div>
 
           <div className="nextpage">
