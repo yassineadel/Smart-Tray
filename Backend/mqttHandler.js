f33316e6460874c7abfca099a8685fb1b15031f0
salwa-galal <salwa.wafaa05@eng-st.cu.edu.eg>
2025-05-25 04:38:11 +0300

    tranport from LAN to WAN

--- Backend/mqttHandler.js
+++ Backend/mqttHandler.js
@@ -1,8 +1,13 @@
 const mqtt = require('mqtt');
 const dataStore = require('./models/dataStore');
+const brokerIP = 'mqtt://41.176.34.155:1883';
+const options = {
+  username: 'myuser',
+  password: 'Talaga1234',
+};
 
 
-const client = mqtt.connect('mqtt://192.168.105.100:1883');
+const client = mqtt.connect(brokerIP, options);
 
 client.on('connect', () => {
   console.log('✅ Connected to MQTT broker');
